eec8c8ba33b8a7acfcb038ecf353dd766e3349de
kei-nishikawa <xnkcelebrate48x@gmail.com>
2021-12-30 21:58:09 +0900

    test

---
+++ lesson.py
@@ -0,0 +1,162 @@
+# num:int =1
+# name:str = "1"
+# is_ok=True
+# print(num,type(num))
+# print(name,type(name))
+# print(is_ok ,type(is_ok))
+# newNum = int(name)
+# print(num,type(newNum))
+# print("hi","mike",sep=",",end=".\n")
+
+# import math
+# result = math.sqrt(25)
+# print(result)
+# print(math.log2(10))
+# print(help(math))
+# print("############")
+# print("""\
+# test
+# aiueo\
+# """)
+# print("############")
+
+# print("test"*2+"aiuoe")
+# s=("aaaaaaaaaaaaaaaaaa"
+# "bbbbbbbbbbbbbbbbbb")
+
+# word = "python"
+# print(word[2:])
+# word="J"+word[1:]
+# print(word)
+
+# n=len(word)
+# print(n)
+# word = "python"
+# # print(word[:2])
+# # print(word[2:])
+
+# word = "J"+word[1:]
+# print(word)
+
+# s = "My name is Mike"
+# print(s)
+# is_start = s.startswith("My")
+# print(is_start)
+# print(s.find("Mike"))
+# print( s.count("Mike"))
+
+
+# r = [1,2,3,4,5,1,2,3]
+# print(r.index(3,3))
+# print(r.count(3))
+
+# if 5 in r:
+#   print("ok")
+
+# r.sort(reverse=True)
+# print(r)
+# r.reverse()
+# print(r)
+
+# s = "My name is mike"
+# to_split = s.split(" ")
+# print(to_split)
+
+# x = " ".join(to_split)
+# print(x)
+
+# i = [1,2,3,4,5]
+# j=i
+# j[0]=100
+# print("j",j)
+# print("i",i)
+# x = [1,2,3,4,5]
+# # y=x.copy()
+# y=x[:]
+
+# y[0]=100
+
+# print("y",y)
+# print("x",x)
+
+# X=20
+# Y=X
+# Y=5
+# print(X)
+# print(Y)
+
+# X=["a","b","c"]
+# Y=X[:]
+# Y[0]="d"
+# print(id(X))
+# print(id(Y))
+# print(X)
+# print(Y)
+# seat = []
+# min = 0
+# max = 5
+# min <= len(seat) < max
+
+# t = (1, 2, 3, 4, 1, 2)
+# print(type(t))
+# t[0]=100
+# chose_from_two = ("A", "B", "C")
+
+# answer = []
+# answer.append("A")
+# answer.append("C")
+
+# print(chose_from_two)
+# print(answer)
+
+# l = [["apple", 100], ["banana", 200], ["orange", 300]]
+
+# fruits = {
+#     "apple": 100,
+#     "banana": 200,
+#     "orange": 300
+# }
+# print(fruits["apple"])
+
+# my_friends = {"a", "b", "c"}
+# A_friends = {"b", "d", "F"}
+# print(my_friends & A_friends)
+
+# f = ["apple", "banana", "apple", "banana"]
+# kind = set(f)
+# print(kind)
+
+# print(f[1])
+
+# s = "ssssss" \
+#     + "bbbbb"
+# x =1 + 1+1+1+1+1+1
+# +2+2+2+2+2+2+2
+# print(x)
+
+# x = 0
+# if x < 0:
+#     print("negative")
+# elif x == 0:
+#     print("zero")
+# else:
+#     print("positive")
+
+# a = 5
+# b = 10
+
+# if a > 0:
+#     print("a is positive")
+#     if b > 0:
+#         print("b is positive")
+
+# y = [1,2,3]
+# x = 1
+# if x in y:
+#     print("in")
+# if 100 not in y:
+#     print("not in")
+
+# is_ok =True
+# if not is_ok:
+#     print("ok")
